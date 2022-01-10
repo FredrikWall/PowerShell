8080775a85015561b53a2e110329aba4543c55e9
Fredrik Wall <wall.fredrik@gmail.com>
2022-01-10 22:54:46 +0100

    Add files via upload

--- Applications/Invoke-DownloadApplication.ps1
+++ Applications/Invoke-DownloadApplication.ps1
@@ -2,38 +2,27 @@ function Invoke-DownloadApplication {
     <#
         .SYNOPSIS
             Download application
-        
         .DESCRIPTION
             Download application with better speed
-        
         .PARAMETER Url
             URL to the application to be downloaded
-            
         .PARAMETER Destination
             Destination of the application
-        
         .EXAMPLE
             PS C:\> Invoke-DownloadApplication -Url "https://www.apple.com/itunes/download/win64" -Destination "$env:TEMP\iTunes64Setup.exe"
             This will download the latest version of iTunes to the temp folder.
-        
         .NOTES
-            Author:     Fredrik Wall
-            Email:      fredrik.powershell@gmail.com
-            Created:    2018-02-08
-            Changed:    2021-01-13
-            Version:    1.1
+            NAME:      	Invoke-DownloadApplication
+            AUTHOR:    	Fredrik Wall, fredrik.powershell@gmail.com
+            VERSION:    1.1
+            CREATED:	02/08/2018
+            CHANGED:    01/12/2021
 
             Changes:
             1.1
             - Will use old $ProgressPreference settings when done downloading instead of allways use Continue.
             - Will write exeption message as well if error.
-        
-        .LINK
-            Latest version of the function can be found here.
-            https://github.com/FredrikWall/PowerShell
-
     #>
-
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -44,7 +33,7 @@ function Invoke-DownloadApplication {
     
     try {
         # Will change $ProgressPreference to SilentlyContinue (No Progressbar)
-        # This will speed up the download
+        # This will speed up the download if you are using an older Windows PowerShell version.
         if (-not($ProgressPreference -eq "SilentlyContinue")) {
             $OldProgressPreference = $ProgressPreference
             $ProgressPreference = "SilentlyContinue"
@@ -63,3 +52,5 @@ function Invoke-DownloadApplication {
         Write-Output $_.Exception
     }
 }
+
+Invoke-DownloadApplication -Url "https://www.apple.com/itunes/download/win64" -Destination "$env:TEMP\iTunes64Setup.exe"
--- Applications/README.md
+++ Applications/README.md
@@ -1,1 +1,57 @@
-Readme
+# Applications
+
+These are my functions for working with Applications.
+
+All functions are stored in a separate .ps1 file.
+
+With an standard example how to use them.
+
+So almost all of the PowerShell files can be runned as they are.
+If you just call them like on of the examples.
+
+Get-ExeAppInformation.ps1
+------------------
+Gets Application information about a .exe file.
+Supports ProductName, ProductVersion,CompanyName and Language.
+
+Tested with:
+Windows PowerShell 5.1 and PowerShell 7+
+
+![alt text](https://github.com/PowerShellFredrik/PowerShellFunctions/blob/main/Applications/Pictures/Get-ExeAppInformation01.png?raw=true)
+
+Get-InstalledApplications.ps1
+------------------
+Shows all installed applications or a single installed application on a local computer.
+
+Tested with:
+Windows PowerShell 5.1 and PowerShell 7+
+
+![alt text](https://github.com/PowerShellFredrik/PowerShellFunctions/blob/main/Applications/Pictures/Get-InstalledApplications01.png?raw=true)
+
+And It's not problem to use it with an if statement.
+
+![alt text](https://github.com/PowerShellFredrik/PowerShellFunctions/blob/main/Applications/Pictures/Get-InstalledApplications02.png?raw=true)
+
+Get-UninstallString.ps1
+------------------
+Shows uninstall string/strings for installed application/applications.
+
+Tested with:
+Windows PowerShell 5.1 and PowerShell 7+
+
+All uninstall strings
+
+![alt text](https://github.com/PowerShellFredrik/PowerShellFunctions/blob/main/Applications/Pictures/Get-UninstallString01.png?raw=true)
+
+All uninstall strings for Citrix
+
+![alt text](https://github.com/PowerShellFredrik/PowerShellFunctions/blob/main/Applications/Pictures/Get-UninstallString02.png?raw=true)
+
+Uninstall string for Chrome
+
+![alt text](https://github.com/PowerShellFredrik/PowerShellFunctions/blob/main/Applications/Pictures/Get-UninstallString04.png?raw=true)
+
+Invoke-DownloadApplication.ps1
+------------------
+It's a simple function for downloading applications from the internet.
+
